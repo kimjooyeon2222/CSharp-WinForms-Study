48bbc6fb81d581232cccc11faf0ac470e722a062
JOO <zpffjtm@naver.com>
2025-01-30 00:09:03 +0900

    Re-add Project2 folder

---
+++ Project2/Study_Loop/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study_Loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            testboxResult.Text = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            int iResult = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                iResult += i;
+                sb.Append(string.Format("1에서 {0}까지 더하면 {1}\r\n", i, iResult));
+            }
+
+            testboxResult.Text = sb.ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
