ae48e1449deaeb7b6a7a48d475fba691554a0f27
JOO <zpffjtm@naver.com>
2025-01-31 12:56:41 +0900

    Implement a code with struct tense and drawing UI

---
+++ Sturct_Study/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Sturct_Study
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.pboxRed = new System.Windows.Forms.PictureBox();
+            this.pboxOrange = new System.Windows.Forms.PictureBox();
+            this.pboxYellow = new System.Windows.Forms.PictureBox();
+            this.pboxNone = new System.Windows.Forms.PictureBox();
+            this.rdoPlayer1 = new System.Windows.Forms.RadioButton();
+            this.rdoPlayer2 = new System.Windows.Forms.RadioButton();
+            this.lboxResult1 = new System.Windows.Forms.ListBox();
+            this.lboxResult2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxOrange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxYellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(20, 21);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(558, 24);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "- Player는 한 번씩 돌아가면서 선택을 하게 됩니다.";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(20, 66);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(796, 24);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "- 각 5회 진행 후 해, 달, 별의 숫자의 합이 가장 높은 사람이 승리합니다. ";
+            this.lbl2.Click += new System.EventHandler(this.lbl2_Click);
+            // 
+            // pboxRed
+            // 
+            this.pboxRed.BackColor = System.Drawing.Color.Red;
+            this.pboxRed.Location = new System.Drawing.Point(24, 111);
+            this.pboxRed.Name = "pboxRed";
+            this.pboxRed.Size = new System.Drawing.Size(170, 203);
+            this.pboxRed.TabIndex = 2;
+            this.pboxRed.TabStop = false;
+            this.pboxRed.Click += new System.EventHandler(this.pboxRed_Click);
+            // 
+            // pboxOrange
+            // 
+            this.pboxOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pboxOrange.Location = new System.Drawing.Point(200, 111);
+            this.pboxOrange.Name = "pboxOrange";
+            this.pboxOrange.Size = new System.Drawing.Size(170, 203);
+            this.pboxOrange.TabIndex = 3;
+            this.pboxOrange.TabStop = false;
+            // 
+            // pboxYellow
+            // 
+            this.pboxYellow.BackColor = System.Drawing.Color.Yellow;
+            this.pboxYellow.Location = new System.Drawing.Point(376, 111);
+            this.pboxYellow.Name = "pboxYellow";
+            this.pboxYellow.Size = new System.Drawing.Size(170, 203);
+            this.pboxYellow.TabIndex = 4;
+            this.pboxYellow.TabStop = false;
+            this.pboxYellow.Click += new System.EventHandler(this.pboxYellow_Click);
+            // 
+            // pboxNone
+            // 
+            this.pboxNone.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pboxNone.Location = new System.Drawing.Point(552, 111);
+            this.pboxNone.Name = "pboxNone";
+            this.pboxNone.Size = new System.Drawing.Size(170, 203);
+            this.pboxNone.TabIndex = 5;
+            this.pboxNone.TabStop = false;
+            // 
+            // rdoPlayer1
+            // 
+            this.rdoPlayer1.AutoSize = true;
+            this.rdoPlayer1.Location = new System.Drawing.Point(24, 335);
+            this.rdoPlayer1.Name = "rdoPlayer1";
+            this.rdoPlayer1.Size = new System.Drawing.Size(117, 28);
+            this.rdoPlayer1.TabIndex = 6;
+            this.rdoPlayer1.TabStop = true;
+            this.rdoPlayer1.Text = "Player1";
+            this.rdoPlayer1.UseVisualStyleBackColor = true;
+            // 
+            // rdoPlayer2
+            // 
+            this.rdoPlayer2.AutoSize = true;
+            this.rdoPlayer2.Location = new System.Drawing.Point(376, 335);
+            this.rdoPlayer2.Name = "rdoPlayer2";
+            this.rdoPlayer2.Size = new System.Drawing.Size(117, 28);
+            this.rdoPlayer2.TabIndex = 7;
+            this.rdoPlayer2.TabStop = true;
+            this.rdoPlayer2.Text = "Player2";
+            this.rdoPlayer2.UseVisualStyleBackColor = true;
+            // 
+            // lboxResult1
+            // 
+            this.lboxResult1.FormattingEnabled = true;
+            this.lboxResult1.ItemHeight = 24;
+            this.lboxResult1.Location = new System.Drawing.Point(24, 382);
+            this.lboxResult1.Name = "lboxResult1";
+            this.lboxResult1.Size = new System.Drawing.Size(346, 124);
+            this.lboxResult1.TabIndex = 8;
+            // 
+            // lboxResult2
+            // 
+            this.lboxResult2.FormattingEnabled = true;
+            this.lboxResult2.ItemHeight = 24;
+            this.lboxResult2.Location = new System.Drawing.Point(376, 382);
+            this.lboxResult2.Name = "lboxResult2";
+            this.lboxResult2.Size = new System.Drawing.Size(346, 124);
+            this.lboxResult2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 564);
+            this.Controls.Add(this.lboxResult2);
+            this.Controls.Add(this.lboxResult1);
+            this.Controls.Add(this.rdoPlayer2);
+            this.Controls.Add(this.rdoPlayer1);
+            this.Controls.Add(this.pboxNone);
+            this.Controls.Add(this.pboxYellow);
+            this.Controls.Add(this.pboxOrange);
+            this.Controls.Add(this.pboxRed);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pboxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxOrange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxYellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.PictureBox pboxRed;
+        private System.Windows.Forms.PictureBox pboxOrange;
+        private System.Windows.Forms.PictureBox pboxYellow;
+        private System.Windows.Forms.PictureBox pboxNone;
+        private System.Windows.Forms.RadioButton rdoPlayer1;
+        private System.Windows.Forms.RadioButton rdoPlayer2;
+        private System.Windows.Forms.ListBox lboxResult1;
+        private System.Windows.Forms.ListBox lboxResult2;
+    }
+}
+
---
+++ Sturct_Study/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sturct_Study
+{
+    public partial class Form1 : Form
+    {
+
+        struct structPlayer
+        {
+            public int iCount;//player가 몇 회 진행 중인지
+            public int iRed;// red에 대한 값
+            public int iOrange;// orange에 대한 값
+            public int iYellow;// yellow에 대한 값
+
+            public int iColorSum; // sum of red, orange, yellow
+
+            //값을 더해서 반환
+            public int ColorSum(int iRed, int iOrange, int iYellow)
+            {
+                return iRed + iOrange + iYellow;
+            }
+
+            public string ResultText()
+            {
+                return string.Format("{0}회) 빨강:{1}, 주황: {2}, 노랑: {3} => 합계는 {4}입니다.", iCount, iRed, iOrange, iYellow, iColorSum);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pboxRed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pboxYellow_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
