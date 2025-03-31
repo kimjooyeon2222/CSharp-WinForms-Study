7c2275ecc5b7ccb28c60829d27c67874d4e32c70
JOO <zpffjtm@naver.com>
2025-03-31 17:04:57 +0900

    Implement the pbox_Click method

--- Project10/List/Form1.cs
+++ Project10/List/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,13 @@ namespace List
 {
     public partial class Form1 : Form
     {
+
+        List<string> _strList = new List<string>();
+        
+        ArrayList _arList = new ArrayList ();
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -26,5 +34,41 @@ namespace List
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbox_Click(object sender, EventArgs e)
+        {
+            PictureBox pbox = sender as PictureBox;
+            string strSelectText = string.Empty;
+
+            switch (pbox.Name)
+            {
+                case "pbox1":
+                    strSelectText = "cake";
+                    break;
+
+                case "pbox2":
+                    strSelectText = "burger";
+                    break;
+
+                case "pbox3":
+                    strSelectText = "pizza";
+                    break;
+
+                case "pbox4":
+                    strSelectText = "ice";
+                    break;
+            }
+
+            _strList.Add(strSelectText);
+            /*_arList.Add(strSelectText);
+            _arList.Add(1);*/
+
+        }
+
     }
 }
