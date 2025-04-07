50223600b8439c958c3c406219c0c7df1a67af59
JOO <zpffjtm@naver.com>
2025-04-07 23:52:12 +0900

    Change font size

--- Project11/Dictionary/Form1.Designer.cs
+++ Project11/Dictionary/Form1.Designer.cs
@@ -60,9 +60,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Location = new System.Drawing.Point(24, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(370, 24);
+            this.label1.Size = new System.Drawing.Size(387, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "※ 선호하는 항목을 선택해주세요";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -87,13 +87,13 @@
             this.tableLayoutPanel1.Controls.Add(this.pbox2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.pbox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.lblPick1, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(26, 82);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 82);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(762, 219);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(821, 219);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // label4
@@ -103,9 +103,9 @@
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(193, 189);
+            this.label4.Location = new System.Drawing.Point(208, 189);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(184, 30);
+            this.label4.Size = new System.Drawing.Size(199, 30);
             this.label4.TabIndex = 11;
             this.label4.Text = "혜원";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -119,7 +119,7 @@
             this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label5.Location = new System.Drawing.Point(3, 189);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(184, 30);
+            this.label5.Size = new System.Drawing.Size(199, 30);
             this.label5.TabIndex = 10;
             this.label5.Text = "주연";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -131,9 +131,9 @@
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label6.Location = new System.Drawing.Point(573, 189);
+            this.label6.Location = new System.Drawing.Point(618, 189);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(186, 30);
+            this.label6.Size = new System.Drawing.Size(200, 30);
             this.label6.TabIndex = 9;
             this.label6.Text = "민정";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -145,9 +145,9 @@
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label7.Location = new System.Drawing.Point(383, 189);
+            this.label7.Location = new System.Drawing.Point(413, 189);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(184, 30);
+            this.label7.Size = new System.Drawing.Size(199, 30);
             this.label7.TabIndex = 8;
             this.label7.Text = "경동";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -158,9 +158,9 @@
             this.lblPick4.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.lblPick4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblPick4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblPick4.Location = new System.Drawing.Point(573, 159);
+            this.lblPick4.Location = new System.Drawing.Point(618, 159);
             this.lblPick4.Name = "lblPick4";
-            this.lblPick4.Size = new System.Drawing.Size(186, 30);
+            this.lblPick4.Size = new System.Drawing.Size(200, 30);
             this.lblPick4.TabIndex = 7;
             this.lblPick4.Text = "0";
             this.lblPick4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -171,9 +171,9 @@
             this.lblPick3.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.lblPick3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblPick3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblPick3.Location = new System.Drawing.Point(383, 159);
+            this.lblPick3.Location = new System.Drawing.Point(413, 159);
             this.lblPick3.Name = "lblPick3";
-            this.lblPick3.Size = new System.Drawing.Size(184, 30);
+            this.lblPick3.Size = new System.Drawing.Size(199, 30);
             this.lblPick3.TabIndex = 6;
             this.lblPick3.Text = "0";
             this.lblPick3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -184,9 +184,9 @@
             this.lblPick2.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.lblPick2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblPick2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblPick2.Location = new System.Drawing.Point(193, 159);
+            this.lblPick2.Location = new System.Drawing.Point(208, 159);
             this.lblPick2.Name = "lblPick2";
-            this.lblPick2.Size = new System.Drawing.Size(184, 30);
+            this.lblPick2.Size = new System.Drawing.Size(199, 30);
             this.lblPick2.TabIndex = 5;
             this.lblPick2.Text = "0";
             this.lblPick2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -195,9 +195,9 @@
             // 
             this.pbox4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pbox4.Image = global::List.Properties.Resources._14;
-            this.pbox4.Location = new System.Drawing.Point(573, 3);
+            this.pbox4.Location = new System.Drawing.Point(618, 3);
             this.pbox4.Name = "pbox4";
-            this.pbox4.Size = new System.Drawing.Size(186, 153);
+            this.pbox4.Size = new System.Drawing.Size(200, 153);
             this.pbox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbox4.TabIndex = 3;
             this.pbox4.TabStop = false;
@@ -207,9 +207,9 @@
             // 
             this.pbox3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pbox3.Image = global::List.Properties.Resources._13;
-            this.pbox3.Location = new System.Drawing.Point(383, 3);
+            this.pbox3.Location = new System.Drawing.Point(413, 3);
             this.pbox3.Name = "pbox3";
-            this.pbox3.Size = new System.Drawing.Size(184, 153);
+            this.pbox3.Size = new System.Drawing.Size(199, 153);
             this.pbox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbox3.TabIndex = 2;
             this.pbox3.TabStop = false;
@@ -219,9 +219,9 @@
             // 
             this.pbox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pbox2.Image = global::List.Properties.Resources._12;
-            this.pbox2.Location = new System.Drawing.Point(193, 3);
+            this.pbox2.Location = new System.Drawing.Point(208, 3);
             this.pbox2.Name = "pbox2";
-            this.pbox2.Size = new System.Drawing.Size(184, 153);
+            this.pbox2.Size = new System.Drawing.Size(199, 153);
             this.pbox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbox2.TabIndex = 1;
             this.pbox2.TabStop = false;
@@ -233,7 +233,7 @@
             this.pbox1.Image = global::List.Properties.Resources._11;
             this.pbox1.Location = new System.Drawing.Point(3, 3);
             this.pbox1.Name = "pbox1";
-            this.pbox1.Size = new System.Drawing.Size(184, 153);
+            this.pbox1.Size = new System.Drawing.Size(199, 153);
             this.pbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbox1.TabIndex = 0;
             this.pbox1.TabStop = false;
@@ -247,7 +247,7 @@
             this.lblPick1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.lblPick1.Location = new System.Drawing.Point(3, 159);
             this.lblPick1.Name = "lblPick1";
-            this.lblPick1.Size = new System.Drawing.Size(184, 30);
+            this.lblPick1.Size = new System.Drawing.Size(199, 30);
             this.lblPick1.TabIndex = 4;
             this.lblPick1.Text = "0";
             this.lblPick1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -255,9 +255,10 @@
             // lblTotalCount
             // 
             this.lblTotalCount.AutoSize = true;
-            this.lblTotalCount.Location = new System.Drawing.Point(213, 369);
+            this.lblTotalCount.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTotalCount.Location = new System.Drawing.Point(229, 369);
             this.lblTotalCount.Name = "lblTotalCount";
-            this.lblTotalCount.Size = new System.Drawing.Size(59, 24);
+            this.lblTotalCount.Size = new System.Drawing.Size(64, 24);
             this.lblTotalCount.TabIndex = 2;
             this.lblTotalCount.Text = "0 / 0";
             this.lblTotalCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -265,9 +266,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 369);
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(31, 369);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(178, 24);
+            this.label3.Size = new System.Drawing.Size(187, 24);
             this.label3.TabIndex = 3;
             this.label3.Text = "투표 진행 현황:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -276,9 +278,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 405);
+            this.label2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(30, 405);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(397, 24);
+            this.label2.Size = new System.Drawing.Size(421, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "※ List의 Data를 화면에 보여줍니다.";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -287,17 +290,17 @@
             // 
             this.dgViewList.BackgroundColor = System.Drawing.Color.DarkSlateBlue;
             this.dgViewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgViewList.Location = new System.Drawing.Point(137, 471);
+            this.dgViewList.Location = new System.Drawing.Point(148, 471);
             this.dgViewList.Name = "dgViewList";
             this.dgViewList.RowHeadersWidth = 82;
             this.dgViewList.RowTemplate.Height = 37;
-            this.dgViewList.Size = new System.Drawing.Size(543, 375);
+            this.dgViewList.Size = new System.Drawing.Size(585, 375);
             this.dgViewList.TabIndex = 5;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(216, 331);
+            this.label9.Location = new System.Drawing.Point(233, 331);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(0, 24);
             this.label9.TabIndex = 9;
@@ -306,9 +309,10 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(29, 331);
+            this.label10.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label10.Location = new System.Drawing.Point(31, 331);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(98, 24);
+            this.label10.Size = new System.Drawing.Size(103, 24);
             this.label10.TabIndex = 8;
             this.label10.Text = "투표자: ";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -316,18 +320,19 @@
             // lblPlayerName
             // 
             this.lblPlayerName.AutoSize = true;
-            this.lblPlayerName.Location = new System.Drawing.Point(133, 331);
+            this.lblPlayerName.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPlayerName.Location = new System.Drawing.Point(143, 331);
             this.lblPlayerName.Name = "lblPlayerName";
-            this.lblPlayerName.Size = new System.Drawing.Size(23, 24);
+            this.lblPlayerName.Size = new System.Drawing.Size(24, 24);
             this.lblPlayerName.TabIndex = 7;
             this.lblPlayerName.Text = "-";
             this.lblPlayerName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 897);
+            this.ClientSize = new System.Drawing.Size(877, 897);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.lblPlayerName);
@@ -337,6 +342,7 @@
             this.Controls.Add(this.lblTotalCount);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
