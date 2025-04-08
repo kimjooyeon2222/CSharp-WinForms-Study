30d445c6d240d71c1de0a5fd8966057240f411b5
JOO <zpffjtm@naver.com>
2025-04-08 16:14:28 +0900

    Implement the Reg UI

--- Project12/DataTable_study/Form1.Designer.cs
+++ Project12/DataTable_study/Form1.Designer.cs
@@ -29,52 +29,37 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxRegClass = new System.Windows.Forms.ComboBox();
+            this.tboxRegName = new System.Windows.Forms.TextBox();
+            this.rdoRegSexMale = new System.Windows.Forms.RadioButton();
+            this.rdoRegSexFemale = new System.Windows.Forms.RadioButton();
+            this.tboxRegRef = new System.Windows.Forms.TextBox();
+            this.btnReg = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnReg);
+            this.groupBox1.Controls.Add(this.tboxRegRef);
+            this.groupBox1.Controls.Add(this.rdoRegSexFemale);
+            this.groupBox1.Controls.Add(this.rdoRegSexMale);
+            this.groupBox1.Controls.Add(this.tboxRegName);
+            this.groupBox1.Controls.Add(this.cboxRegClass);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(59, 58);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(426, 246);
+            this.groupBox1.Size = new System.Drawing.Size(542, 339);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "등록하기";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "반：";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 94);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "이름：";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(53, 182);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 24);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "특기：";
             // 
             // label4
             // 
@@ -85,11 +70,97 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "성별：";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "특기：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "이름：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "반：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cboxRegClass
+            // 
+            this.cboxRegClass.FormattingEnabled = true;
+            this.cboxRegClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxRegClass.Location = new System.Drawing.Point(141, 53);
+            this.cboxRegClass.Name = "cboxRegClass";
+            this.cboxRegClass.Size = new System.Drawing.Size(143, 32);
+            this.cboxRegClass.TabIndex = 4;
+            // 
+            // tboxRegName
+            // 
+            this.tboxRegName.Location = new System.Drawing.Point(141, 94);
+            this.tboxRegName.Name = "tboxRegName";
+            this.tboxRegName.Size = new System.Drawing.Size(143, 35);
+            this.tboxRegName.TabIndex = 5;
+            this.tboxRegName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // rdoRegSexMale
+            // 
+            this.rdoRegSexMale.AutoSize = true;
+            this.rdoRegSexMale.Location = new System.Drawing.Point(141, 139);
+            this.rdoRegSexMale.Name = "rdoRegSexMale";
+            this.rdoRegSexMale.Size = new System.Drawing.Size(89, 28);
+            this.rdoRegSexMale.TabIndex = 6;
+            this.rdoRegSexMale.TabStop = true;
+            this.rdoRegSexMale.Text = "남자";
+            this.rdoRegSexMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoRegSexFemale
+            // 
+            this.rdoRegSexFemale.AutoSize = true;
+            this.rdoRegSexFemale.Location = new System.Drawing.Point(258, 139);
+            this.rdoRegSexFemale.Name = "rdoRegSexFemale";
+            this.rdoRegSexFemale.Size = new System.Drawing.Size(89, 28);
+            this.rdoRegSexFemale.TabIndex = 7;
+            this.rdoRegSexFemale.TabStop = true;
+            this.rdoRegSexFemale.Text = "여자";
+            this.rdoRegSexFemale.UseVisualStyleBackColor = true;
+            // 
+            // tboxRegRef
+            // 
+            this.tboxRegRef.Location = new System.Drawing.Point(141, 179);
+            this.tboxRegRef.Name = "tboxRegRef";
+            this.tboxRegRef.Size = new System.Drawing.Size(367, 35);
+            this.tboxRegRef.TabIndex = 8;
+            // 
+            // btnReg
+            // 
+            this.btnReg.Location = new System.Drawing.Point(382, 239);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(126, 42);
+            this.btnReg.TabIndex = 9;
+            this.btnReg.Text = "등록";
+            this.btnReg.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 642);
+            this.ClientSize = new System.Drawing.Size(1043, 752);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -106,6 +177,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tboxRegName;
+        private System.Windows.Forms.ComboBox cboxRegClass;
+        private System.Windows.Forms.TextBox tboxRegRef;
+        private System.Windows.Forms.RadioButton rdoRegSexFemale;
+        private System.Windows.Forms.RadioButton rdoRegSexMale;
+        private System.Windows.Forms.Button btnReg;
     }
 }
 
--- Project12/DataTable_study/Form1.cs
+++ Project12/DataTable_study/Form1.cs
@@ -16,5 +16,10 @@ namespace DataTable_study
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
