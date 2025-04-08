f95982f808830e192245be9ec9584ef8be38cf69
JOO <zpffjtm@naver.com>
2025-04-08 16:19:41 +0900

    Fully implement the UI

--- Project12/DataTable_study/Form1.Designer.cs
+++ Project12/DataTable_study/Form1.Designer.cs
@@ -29,17 +29,23 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.tboxRegRef = new System.Windows.Forms.TextBox();
+            this.rdoRegSexFemale = new System.Windows.Forms.RadioButton();
+            this.rdoRegSexMale = new System.Windows.Forms.RadioButton();
+            this.tboxRegName = new System.Windows.Forms.TextBox();
+            this.cboxRegClass = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.cboxRegClass = new System.Windows.Forms.ComboBox();
-            this.tboxRegName = new System.Windows.Forms.TextBox();
-            this.rdoRegSexMale = new System.Windows.Forms.RadioButton();
-            this.rdoRegSexFemale = new System.Windows.Forms.RadioButton();
-            this.tboxRegRef = new System.Windows.Forms.TextBox();
-            this.btnReg = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnViewDataDel = new System.Windows.Forms.Button();
+            this.cboxViewClass = new System.Windows.Forms.ComboBox();
+            this.dgViewInfo = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -54,12 +60,70 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(59, 58);
+            this.groupBox1.Location = new System.Drawing.Point(34, 58);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(542, 339);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "등록하기";
+            // 
+            // btnReg
+            // 
+            this.btnReg.Location = new System.Drawing.Point(382, 239);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(126, 42);
+            this.btnReg.TabIndex = 9;
+            this.btnReg.Text = "등록";
+            this.btnReg.UseVisualStyleBackColor = true;
+            // 
+            // tboxRegRef
+            // 
+            this.tboxRegRef.Location = new System.Drawing.Point(141, 179);
+            this.tboxRegRef.Name = "tboxRegRef";
+            this.tboxRegRef.Size = new System.Drawing.Size(367, 35);
+            this.tboxRegRef.TabIndex = 8;
+            // 
+            // rdoRegSexFemale
+            // 
+            this.rdoRegSexFemale.AutoSize = true;
+            this.rdoRegSexFemale.Location = new System.Drawing.Point(258, 139);
+            this.rdoRegSexFemale.Name = "rdoRegSexFemale";
+            this.rdoRegSexFemale.Size = new System.Drawing.Size(89, 28);
+            this.rdoRegSexFemale.TabIndex = 7;
+            this.rdoRegSexFemale.TabStop = true;
+            this.rdoRegSexFemale.Text = "여자";
+            this.rdoRegSexFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoRegSexMale
+            // 
+            this.rdoRegSexMale.AutoSize = true;
+            this.rdoRegSexMale.Location = new System.Drawing.Point(141, 139);
+            this.rdoRegSexMale.Name = "rdoRegSexMale";
+            this.rdoRegSexMale.Size = new System.Drawing.Size(89, 28);
+            this.rdoRegSexMale.TabIndex = 6;
+            this.rdoRegSexMale.TabStop = true;
+            this.rdoRegSexMale.Text = "남자";
+            this.rdoRegSexMale.UseVisualStyleBackColor = true;
+            // 
+            // tboxRegName
+            // 
+            this.tboxRegName.Location = new System.Drawing.Point(141, 94);
+            this.tboxRegName.Name = "tboxRegName";
+            this.tboxRegName.Size = new System.Drawing.Size(143, 35);
+            this.tboxRegName.TabIndex = 5;
+            this.tboxRegName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cboxRegClass
+            // 
+            this.cboxRegClass.FormattingEnabled = true;
+            this.cboxRegClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxRegClass.Location = new System.Drawing.Point(141, 53);
+            this.cboxRegClass.Name = "cboxRegClass";
+            this.cboxRegClass.Size = new System.Drawing.Size(143, 32);
+            this.cboxRegClass.TabIndex = 4;
             // 
             // label4
             // 
@@ -98,74 +162,64 @@
             this.label1.Text = "반：";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // cboxRegClass
+            // groupBox2
             // 
-            this.cboxRegClass.FormattingEnabled = true;
-            this.cboxRegClass.Items.AddRange(new object[] {
+            this.groupBox2.Controls.Add(this.dgViewInfo);
+            this.groupBox2.Controls.Add(this.btnViewDataDel);
+            this.groupBox2.Controls.Add(this.cboxViewClass);
+            this.groupBox2.Location = new System.Drawing.Point(34, 417);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(972, 518);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "내용확인";
+            // 
+            // btnViewDataDel
+            // 
+            this.btnViewDataDel.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnViewDataDel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnViewDataDel.Location = new System.Drawing.Point(823, 46);
+            this.btnViewDataDel.Name = "btnViewDataDel";
+            this.btnViewDataDel.Size = new System.Drawing.Size(126, 42);
+            this.btnViewDataDel.TabIndex = 9;
+            this.btnViewDataDel.Text = "삭제";
+            this.btnViewDataDel.UseVisualStyleBackColor = false;
+            // 
+            // cboxViewClass
+            // 
+            this.cboxViewClass.FormattingEnabled = true;
+            this.cboxViewClass.Items.AddRange(new object[] {
             "1반",
             "2반",
             "3반"});
-            this.cboxRegClass.Location = new System.Drawing.Point(141, 53);
-            this.cboxRegClass.Name = "cboxRegClass";
-            this.cboxRegClass.Size = new System.Drawing.Size(143, 32);
-            this.cboxRegClass.TabIndex = 4;
+            this.cboxViewClass.Location = new System.Drawing.Point(6, 46);
+            this.cboxViewClass.Name = "cboxViewClass";
+            this.cboxViewClass.Size = new System.Drawing.Size(157, 32);
+            this.cboxViewClass.TabIndex = 4;
             // 
-            // tboxRegName
+            // dgViewInfo
             // 
-            this.tboxRegName.Location = new System.Drawing.Point(141, 94);
-            this.tboxRegName.Name = "tboxRegName";
-            this.tboxRegName.Size = new System.Drawing.Size(143, 35);
-            this.tboxRegName.TabIndex = 5;
-            this.tboxRegName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // rdoRegSexMale
-            // 
-            this.rdoRegSexMale.AutoSize = true;
-            this.rdoRegSexMale.Location = new System.Drawing.Point(141, 139);
-            this.rdoRegSexMale.Name = "rdoRegSexMale";
-            this.rdoRegSexMale.Size = new System.Drawing.Size(89, 28);
-            this.rdoRegSexMale.TabIndex = 6;
-            this.rdoRegSexMale.TabStop = true;
-            this.rdoRegSexMale.Text = "남자";
-            this.rdoRegSexMale.UseVisualStyleBackColor = true;
-            // 
-            // rdoRegSexFemale
-            // 
-            this.rdoRegSexFemale.AutoSize = true;
-            this.rdoRegSexFemale.Location = new System.Drawing.Point(258, 139);
-            this.rdoRegSexFemale.Name = "rdoRegSexFemale";
-            this.rdoRegSexFemale.Size = new System.Drawing.Size(89, 28);
-            this.rdoRegSexFemale.TabIndex = 7;
-            this.rdoRegSexFemale.TabStop = true;
-            this.rdoRegSexFemale.Text = "여자";
-            this.rdoRegSexFemale.UseVisualStyleBackColor = true;
-            // 
-            // tboxRegRef
-            // 
-            this.tboxRegRef.Location = new System.Drawing.Point(141, 179);
-            this.tboxRegRef.Name = "tboxRegRef";
-            this.tboxRegRef.Size = new System.Drawing.Size(367, 35);
-            this.tboxRegRef.TabIndex = 8;
-            // 
-            // btnReg
-            // 
-            this.btnReg.Location = new System.Drawing.Point(382, 239);
-            this.btnReg.Name = "btnReg";
-            this.btnReg.Size = new System.Drawing.Size(126, 42);
-            this.btnReg.TabIndex = 9;
-            this.btnReg.Text = "등록";
-            this.btnReg.UseVisualStyleBackColor = true;
+            this.dgViewInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgViewInfo.Location = new System.Drawing.Point(6, 110);
+            this.dgViewInfo.Name = "dgViewInfo";
+            this.dgViewInfo.RowHeadersWidth = 82;
+            this.dgViewInfo.RowTemplate.Height = 37;
+            this.dgViewInfo.Size = new System.Drawing.Size(960, 354);
+            this.dgViewInfo.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 752);
+            this.ClientSize = new System.Drawing.Size(1043, 959);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -183,6 +237,10 @@
         private System.Windows.Forms.RadioButton rdoRegSexFemale;
         private System.Windows.Forms.RadioButton rdoRegSexMale;
         private System.Windows.Forms.Button btnReg;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnViewDataDel;
+        private System.Windows.Forms.ComboBox cboxViewClass;
+        private System.Windows.Forms.DataGridView dgViewInfo;
     }
 }
 
