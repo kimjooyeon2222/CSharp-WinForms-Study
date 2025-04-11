b764552e27c1a55a99eab6e0b3193009da34d4d1
JOO <zpffjtm@naver.com>
2025-04-11 15:24:51 +0900

    Fully implement the cboxViewClass method

--- Project12/DataTable_study/Form1.cs
+++ Project12/DataTable_study/Form1.cs
@@ -23,21 +23,48 @@ namespace DataTable_study
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+           
+
+
+        }
+
+        private void btnReg_Click(object sender, EventArgs e)
+        {
+            bool bCheckisTable = false;
+
+            if (ds.Tables.Contains(cboxRegClass.Text))
+            {
+                bCheckisTable = true;
+            }
+
+
+
             DataTable dt = null;
-            dt = new DataTable(cboxRegClass.Text);
 
-            DataColumn colName = new DataColumn("NAME", typeof(string));
-            DataColumn colSex = new DataColumn("SEX", typeof(string));
-            DataColumn colRef = new DataColumn("REF", typeof(string));
+            if (!bCheckisTable)
+            {
+                dt = new DataTable(cboxRegClass.Text);
 
-            dt.Columns.Add(colName);
-            dt.Columns.Add(colSex);
-            dt.Columns.Add(colRef);
+                DataColumn colName = new DataColumn("NAME", typeof(string));
+                DataColumn colSex = new DataColumn("SEX", typeof(string));
+                DataColumn colRef = new DataColumn("REF", typeof(string));
+
+                dt.Columns.Add(colName);
+                dt.Columns.Add(colSex);
+                dt.Columns.Add(colRef);
+            }
+            else
+            {
+                dt = ds.Tables[cboxRegClass.Text];
+            }
+
 
 
             //Row 자료를 등록
             DataRow row = dt.NewRow();
+
             row["NAME"] = tboxRegName.Text;
+
             if (rdoRegSexMale.Checked)
             {
                 row["SEX"] = "남자";
@@ -48,21 +75,34 @@ namespace DataTable_study
             }
 
             row["SEX"] = tboxRegName.Text;
+
             row["NAME"] = tboxRegName.Text;
 
             row["REF"] = tboxRegRef.Text;
 
+
             // 생성한 Row를 Table에 등록
 
-            dt.Rows.Add(row);
+            /* dt.Rows.Add(row);
+             if (bCheckisTable)
+             {
+                 ds.Tables.Remove(cboxRegClass.Text);
+                 ds.Tables.Add(dt);
+             }
+             ds.Tables.Add(dt);*/
 
-            ds.Tables.Add(dt);
+            if (bCheckisTable)
+            {
+                ds.Tables[cboxRegClass.Text].Rows.Add(row);
+            }
+            else
+            {
+                dt.Rows.Add(row);
+                ds.Tables.Add(dt);
 
+            }
 
-        }
-
-        private void btnReg_Click(object sender, EventArgs e)
-        {
+            cboxViewClass_SelectedIndexChanged(this, null);
 
         }
 
@@ -73,8 +113,18 @@ namespace DataTable_study
 
         private void cboxViewClass_SelectedIndexChanged(object sender, EventArgs e)
         {
+            dgViewInfo.DataSource = ds.Tables[cboxViewClass.Text];
+
+
+            //DatagridView Cell 정렬 및 Number를 적용
+            foreach (DataGridViewRow oRow in dgViewInfo.Rows)
+            {
+                oRow.HeaderCell.Value = oRow.Index.ToString();
+            }
+            dgViewInfo.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
 
         }
+
 
         private void cboxRegClass_SelectedIndexChanged(object sender, EventArgs e)
         {
